aef638d5a6574f71aefb7b008a7668e1d0b5acb3
7exx <marco.tex@live.com>
2017-05-18 20:04:24 +0200

    inizio struttur comunicazione RSA (RFC)

--- alice/cifra_invia.py
+++ alice/cifra_invia.py
@@ -6,9 +6,15 @@ import os
 import socket
 from support import algorithm
 
-IP_DEST = '192.168.35.128'
+IP_DEST = '192.168.242.130'
 PORT_DEST = 12345
 FILE_ORIG = 'f22_raptor.jpg'
+'''
+l'idea di base e' che per cominciare a dialogare, alice manda a bob
+md5 del file e quindi solo dopo bob risponde con la chiave pubblica
+quindi alice puo' cominciare a cifrare il file con le chiavi ricevute
+'''
+
 
 if __name__ == '__main__':
 
@@ -18,6 +24,12 @@ if __name__ == '__main__':
     md5_orig = algorithm.get_md5(FILE_ORIG)
     sock.send(md5_orig.encode())
     print('md5 sent: ', md5_orig)
+
+    n = sock.recv(10).decode()
+    e = sock.recv(10).decode()
+
+
+    sock.close()
 
 
 
--- bob/decifra_ricevi.py
+++ bob/decifra_ricevi.py
@@ -6,7 +6,7 @@ import os
 import socket
 from support import algorithm
 
-IP_REC = '192.168.35.128'
+IP_REC = '192.168.242.130'
 PORT_REC = 12345
 
 if __name__ == '__main__':
@@ -21,10 +21,17 @@ if __name__ == '__main__':
     md5_orig = clientsocket.recv(algorithm.MD5_LENGTH)
     print('md5 originale: ', md5_orig)
 
-    (pub_key, priv_key) = rsa.newkeys(512)
+    (pub_key, priv_key) = rsa.newkeys(32)
     print('n ', pub_key.n, ' e ', pub_key.e)
     print('n ', priv_key.n, ' p ', priv_key.p, ' q ', priv_key.q, ' d ', priv_key.d)
 
-    clientsocket.send(str(pub_key.n))
+    clientsocket.send(str(pub_key.n).zfill(10).encode())
+    clientsocket.send(str(pub_key.e).zfill(10).encode())
+
+
+    clientsocket.close()
+    sock.close()
+
+
 
 
